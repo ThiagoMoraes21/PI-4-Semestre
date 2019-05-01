4f949b8d13654594460b5ffd0a4751b99c56ad8e
Thiago Moraes <thiago4x70@gmail.com>
2019-05-01 18:04:07 -0300

    docummenting

--- public/js/style.js
+++ public/js/style.js
@@ -1,34 +1,39 @@
-// var myDiv = $('.card-text');
-// myDiv.text(myDiv.text().substring(0,90));
-
 window.onload = function() {
     let navbar = document.querySelector('.navbar');
     let isIndexPage = document.querySelector('#indexPage');
-    console.log(isIndexPage);
     
+    // verify if the current page is not the index page
     if (isIndexPage == undefined || isIndexPage == "" || isIndexPage == null) {
+        // if not, adds the class bgColor to the navbar
         navbar.classList.add('bgColor');
     }
 
     window.onscroll = function() {
+        // if the current page is the index page, it chages the color of the navbar on scroll
         if(isIndexPage != undefined && isIndexPage != "" && isIndexPage != null) {
+            // checks if the page was scrolled
             if(document.documentElement.scrollTop >= 150) {
                 navbar.classList.add('bgColor');
             } else {
+                // if it's in the top, take off the class with the color
                 navbar.classList.remove('bgColor');
             }
         }
     }
 
 }
+
+// toggle the content of a section when the button was clicked
 function toggleSection(id) {
     let e = document.getElementById(id);
     let arrow = document.querySelector('.fa-angle-down');
 
+    // if the containter is not visible, make's it visible
     if(e.style.display == 'none'){
         e.style.display = 'block';
         arrow.classList.toggle('arrow-up');
     } else {
+        // else, make's it invisible
         e.style.display = 'none';
         arrow.classList.toggle('arrow-up');
     }
